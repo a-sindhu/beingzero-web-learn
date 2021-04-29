582a2be1bb0be966ba1b3f4581cec34549319f16
a_sindhu <asindhubhargavi@gmail.com>
2021-04-29 10:56:51 +0530

    Added resume handler

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.get("/", function(req, res){
     res.send("Welcome to Sindhu's Basic Site");
 })
 
+app.get("/resume", function(req, res){
+    res.send("Welcome to my resume page");
+})
 
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;

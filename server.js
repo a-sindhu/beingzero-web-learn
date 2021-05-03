304e947227c82851bad27c306346d489c1909ce3
a_sindhu <asindhubhargavi@gmail.com>
2021-05-03 20:12:18 +0530

    Added new files

--- server.js
+++ server.js
@@ -2,17 +2,25 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname+"/frontend"));
+
 //home handler
 app.get("/", function(req, res){
-    res.send("Welcome to Sindhu's Basic Site");
+    let filePathName1=__dirname+"/frontend/html/basic.html"
+    res.sendFile(filePathName1);
+    // res.send("Welcome to Sindhu's Basic Site");
 })
-
-app.use(express.static(__dirname+"/frontend"));
 
 //resume handler
 app.get("/resume", function(req, res){
-    filePathName=__dirname+"/frontend/html/resume.html"
-    res.sendFile(filePathName);
+    let filePathName2=__dirname+"/frontend/html/resume.html"
+    res.sendFile(filePathName2);
+})
+
+//google handler
+app.get("/google", function(req, res){
+    let filePathName3=__dirname+"/frontend/html/google.html"
+    res.sendFile(filePathName3);
 })
 
 // Heroku will automatically set an environment variable called PORT
